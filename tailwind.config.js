e3d1261055faf15d96e382156a3edf637c74e6ee
LeonAli5366 <leonali274784@gmail.com>
2024-12-23 19:05:00 +0600

    test

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
-/** @type {import('tailwindcss').Config} */
+// Import necessary packages
 import animate from "tailwindcss-animate";
-module.exports = {
+
+/** @type {import('tailwindcss').Config} */
+const config = {
   darkMode: ["class"],
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -68,10 +70,8 @@ module.exports = {
       },
       backgroundImage: {
         image1: "url('/images/solar-panels-desktop.jpg')",
-        image2:
-          "url('https://i.postimg.cc/XN13RTRC/Blue-Tech-1-5cfaae031dcfb.avif')",
-        image3:
-          "url('https://i.postimg.cc/tJ5pHSwn/360-F-830647653-OVhah-RNCu1-U7-WZa-Gwn-W5n-Molsm7-AVk-Md.webp')",
+        image2: "url('https://i.postimg.cc/XN13RTRC/Blue-Tech-1-5cfaae031dcfb.avif')",
+        image3: "url('https://i.postimg.cc/tJ5pHSwn/360-F-830647653-OVhah-RNCu1-U7-WZa-Gwn-W5n-Molsm7-AVk-Md.webp')",
         solarPanels2: "url('/images/solar-panels-sec-2.jpg')",
         image5: "url('/images/lights-stay-on-desktop.webp')",
         image6: "url('/images/powering-on-desktop.webp')",
@@ -80,3 +80,6 @@ module.exports = {
   },
   plugins: [animate],
 };
+
+// Export the configuration object
+export default config;
